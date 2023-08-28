e141ab11f7acf9af6f0a92beba8f8d0d1a774c26
pablogd-mx <pablo.diaz@mendix.com>
2023-08-28 12:47:26 +0200

    Refined version with everything but mx4pc connected

--- bash_menu.sh
+++ bash_menu.sh
@@ -1,59 +1,119 @@
 #!/bin/bash
 
-# Function to install k3d and create clusters
-install_k3d_and_create_clusters() {
-    echo "Installing k3d..."
-    # Install k3d installation steps here
+# Function to install prerequisites
+install_prerequisites() {
+    echo "Installing prerequisites..."
+    # Install KinD
+    #GO111MODULE="on" go get sigs.k8s.io/kind@v0.11.1
+    curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-darwin-amd64
+    chmod +x ./kind
+    sudo mv ./kind /usr/local/bin/
+    # Install Helm
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+    chmod +x get_helm.sh
+    ./get_helm.sh
 
-    echo "Creating clusters..."
-    # Create clusters using k3d here
+    echo "Prerequisites installed!"
 }
 
-# Function to install helm and other dependencies
-install_dependencies() {
-    echo "Installing Helm..."
-    # Install Helm and other dependencies here
+# Function to create KinD clusters
+create_clusters() {
+    read -p "Enter the number of clusters to create: " num_clusters
+    read -p "Enter the version of MxOperator to install: " mxversion
+    #Checking if Docker is running
+    echo "Checking if Docker daemon is running, or else..."
+    open /Applications/Docker.app
+    sleep 15
+
+for ((i=1; i<=$num_clusters; i++)); do
+    cluster_name="mx4pc-cluster-$i"
+    namespace_name="pmp-ns-$i"
+    echo "Creating cluster: $cluster_name"
+    kind create cluster --name "$cluster_name"
+    
+    # Install Docker registry using Helm - TODO
+    # kubectl create namespace docker-registry
+    # helm repo add stable https://charts.helm.sh/stable
+    # helm install registry stable/docker-registry --namespace docker-registry
+    # Install PostgreSQL using Helm
+    echo "Installing PostgresSQL"
+        helm repo add bitnami https://charts.bitnami.com/bitnami
+        kubectl create namespace pmp-storage
+        helm install postgres-shared bitnami/postgresql --namespace=pmp-storage --set auth.postgresPassword=Password1\! --set image.tag=12 --set persistence.size=1Gi
+    
+    # Install Minio using Helm  
+    echo "Installing Minio Storage"
+        helm install minio-shared bitnami/minio --namespace=pmp-storage --set auth.rootUser=minioadmin --set auth.rootPassword=Password1\! --set persistence.size=5Gi   
+    echo "Cluster $i created and components installed successfully!"
+
+    # Install Mx4PC Operator ( Standalone)
+     echo "Installing Mx4PC Operator (Standalone)"
+     wget -c https://cdn.mendix.com/mendix-for-private-cloud/mxpc-cli/mxpc-cli-$mxversion.0-macos-amd64.tar.gz 
+     tar -zxvf mxpc-cli*
+     chmod +x *mxpc-cli
+     ./mxpc-cli base-install -n $namespace_name -t generic -m standalone
+
+    echo " *********************"
+
+    echo " Database endpoint is: postgres-shared-postgresql.pmp-storage.svc.cluster.local:5432. Check secrets for credentials"
+    echo " Storage endpoint is: http://minio-shared.pmp-storage.svc.cluster.local:9000. Check secrets for credentials"
+ 
+    echo " *********************"
+
+ done
+
 }
 
-# Function to install Nginx Ingress Controller
-install_nginx_ingress() {
-    echo "Installing Nginx Ingress Controller..."
-    # Install Nginx Ingress Controller here
+# install_mx4pc_standalone() {
+
+# }
+
+install_mx4pc_connected() {
+    echo " Yet to be done"
+
 }
 
-# Function to install local registry with HTTPS
-install_local_registry() {
-    echo "Installing local registry with HTTPS..."
-    # Install local registry with HTTPS here
+delete_all_kind_clusters() {
+    echo "Deleting all KinD clusters..."
+    kind get clusters | while read -r cluster; do
+        kind delete cluster --name "$cluster"
+    done
+    echo "All KinD clusters deleted."
 }
+
 
 # Main menu
 main_menu() {
     clear
     echo "Welcome to the Cluster Setup Script!"
     echo "Select an option:"
-    echo "1. Install k3d and create clusters"
-    echo "2. Install Helm and other dependencies"
-    echo "3. Install Nginx Ingress Controller"
-    echo "4. Install local registry with HTTPS"
-    echo "5. Exit"
+    echo "1. Install prerequisites"
+    echo "2. Create KinD clusters"
+    echo "3. Install & Configure Mx4PC Standalone"
+    echo "4. Install & Configure Mx4PC Connected"
+    echo "5. Delete all clusters"
+  #  echo "4. Deploy Harbor registry with TLS"
+    echo "6. Exit"
 
     read -p "Enter your choice: " choice
 
     case $choice in
         1)
-            install_k3d_and_create_clusters
+            install_prerequisites
             ;;
         2)
-            install_dependencies
+            create_clusters
             ;;
         3)
-            install_nginx_ingress
+            install_mx4pc_standalone
             ;;
         4)
-            install_local_registry
+            install_mx4pc_connected
             ;;
-        5)
+        5) 
+            delete_all_kind_clusters
+            ;;
+        6)
             echo "Exiting..."
             exit 0
             ;;
@@ -70,4 +130,3 @@ main_menu() {
 
 # Start the script
 main_menu
-
